01e2cf9180e16776063827fec8b19b849776ba49
rlop74 <119091421+rlop74@users.noreply.github.com>
2026-02-17 22:42:05 -0500

    Worked on products pages: - updated products list to fetch data from api/db - updated product details to fetch data from api/db

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,23 @@
 import axios from "axios";
 
-// const API_URL = "https://api.escuelajs.co/api/v1/products";
+const API_URL = "https://api.escuelajs.co/api/v1/products";
 
-export const fetchProducts = async () => {};
+export const fetchProducts = async () => {
+    try {
+        const { data } = await axios.get(API_URL)
+        return data;
+    } catch (err) {
+        console.error("Failed to fetch products: ", err)
+        alert("Something went wrong")
+    }
+};
+
+export const fetchSingleProducts = async (id: any) => {
+    try {
+        const { data } = await axios.get(`${API_URL}/${id}`);
+        return data;
+    } catch (err) {
+        console.error("Failed to fetch product: ", err)
+        alert("Something went wrong")
+    }
+}
