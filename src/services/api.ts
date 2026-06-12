5e91f1e028daa069a7b272d75e41dd2d11da783d
Romane Dorrel <romanedorrel@gmail.com>
2026-06-12 12:05:32 -0400

    Ro attempt

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,22 @@
 import axios from "axios";
 
-// const API_URL = "https://api.escuelajs.co/api/v1/products";
+const API_URL = "https://api.escuelajs.co/api/v1/products";
 
-export const fetchProducts = async () => {};
+export const fetchProducts = async () => {
+  const response = await axios.get(API_URL);
+
+  if (!response) {
+    console.error("Failed to Fetch products");
+  }
+  console.log(response);
+  return response;
+};
+
+export const fetchProduct = async (id: string) => {
+  const response = await axios.get(`${API_URL}/${id}`);
+
+  if (!response) {
+    console.error("Failed to fetch product");
+  }
+  return response;
+};
